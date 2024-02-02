a799c1fab5cc24b3d8b398d38c422b6d63ad3e8b
Dnoil <mr.danyavorontsov@yandex.ru>
2024-02-02 16:17:16 +0300

    Prepare HW01

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -10,13 +10,14 @@ import java.io.IOException;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-public class UserServlet extends HttpServlet {
+public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to users");
+        System.out.println("Hello");
+        /*log.debug("redirect to meals");
 
-        response.sendRedirect("users.jsp");
+        response.sendRedirect("meals.jsp");*/
     }
 }
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,27 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html lang="ru">
+<head>
+    <title>Meals</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+<table border=1>
+  <tr>
+    <td>Date</td>
+    <td>Description</td>
+    <td>Calories</td>
+    <td> </td>
+    <td> </td>
+  </tr>
+  <tr>
+    <td>Время</td>
+    <td>Завтрак например</td>
+    <td>300</td>
+    <td>Update</td>
+    <td>Delete</td>
+  </tr>
+</table>
+</body>
+</html>
